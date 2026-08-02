341595586b2a7ed88623b3564ac3e482d27245da
suryfilms-sys <suryfilms@gmail.com>
2026-08-02 17:22:07 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'ide-sticker-prints-v6';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return cached version
+        }
+        return fetch(event.request); // Otherwise fetch from internet
+      })
+  );
+});
